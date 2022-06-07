05727043572c13be1a56758943a15e73b4902bc2
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 19:50:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Creando una función
+
+// Function Declaration
+function saludar(nombre) {
+ console.log('Bienvenido ' + nombre)
+}
+saludar()
+
+// Function expression
+const cliente = function (nombreCliente) {
+ console.log('Mostrando datos del cliente: ' + nombreCliente)
+}
+cliente('Frank')
